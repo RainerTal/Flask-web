a22e0a14cf65770995b3870c37729c9f70f71d0e
Rainer Talvar <rainer.talvar@gmail.com>
2024-09-05 17:28:16 +0300

    Created htmls for all sites and added POST/GET methods for auth.py and created basic forms for sign up and log in

--- website/auth.py
+++ website/auth.py
@@ -1,19 +1,20 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=["GET", "POST"])
 def login():
-    return('<p>Login</p>')
+    data = request.form
+    return render_template("login.html", boolean=True)
 
 @auth.route('/logout')
 def logout():
     return('<p>Logout</p>')
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=["GET", "POST"])
 def signup():
-    return('<p>Sign Up</p>')
+    return render_template("sign_up.html")
 
 @auth.route("/stock")
 def stock():
-    return('<p>Stock</p>')
+    return render_template("stock.html")
