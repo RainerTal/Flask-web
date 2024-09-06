428c82bee9c332fa9c1265d546813650be3a12c7
Rainer Talvar <rainer.talvar@gmail.com>
2024-09-06 17:14:20 +0300

    Created database logic, database init, function to check if database is running if not then start it, created if-statement algorithm for sign up to check for correct values, created sign up/login forms in html

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path 
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'asd'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
@@ -10,4 +17,14 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    from .models import User, DataValues
+
+    create_database(app)
+
     return app
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        with app.app_context():
+            db.create_all
+        print("Database created")
--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
@@ -13,6 +13,21 @@ def logout():
 
 @auth.route('/sign-up', methods=["GET", "POST"])
 def signup():
+    if request.method == 'POST':
+        email = request.form.get("email")
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(firstName) < 2:
+            flash("Name is shorter than 2 characters", category="error")
+        elif password1 != password2:
+            flash("Passwords do not match", category="error")
+        elif len(password1) < 7 or not any(char.isdigit() for char in password1):
+            flash("Password must be at least 7 characters long and contain a number", category="error")
+        else:
+            flash("Account created", category="success")
+
     return render_template("sign_up.html")
 
 @auth.route("/stock")
